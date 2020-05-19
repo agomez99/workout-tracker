cd838c1a4b65915140f6be158828caf9ae7ffd56
agomez99 <austine.gomez@yahoo.com>
2020-05-19 16:29:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // MongoDB
-var MONGOLAB_CYAN_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGOLAB_CYAN_URI, {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false
 })
