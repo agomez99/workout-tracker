09bdb8a82e68d4c5a3bd856de662d2ebc0e79373
Austine Gomez <austine.gomez@yahoo.com>
2020-05-20 21:11:39 -0500

    Update workout.js

--- models/workout.js
+++ models/workout.js
@@ -41,13 +41,11 @@ const workoutSchema = new Schema(
   },
   {
     toJSON: {
-      // include any virtual properties when data is requested
       virtuals: true
     }
   }
 );
 
-// adds a dynamically-created property to schema
 workoutSchema.virtual("totalDuration").get(function () {
   // "reduce" array of exercises down to just the sum of their durations
   return this.exercises.reduce((total, exercise) => {
